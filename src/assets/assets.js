d57bd2c6a805f6b07d713b4977128bd61e4b6a73
Archit Kayal <architkayal@gmail.com>
2025-07-16 13:44:22 +0530

    add assets

---
+++ src/assets/assets.js
@@ -0,0 +1,105 @@
+import logo from './logo.svg'
+import logo_dark  from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.jpg'
+import brand_img from './brand_img'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.jpg'
+import profile_img_2 from './profile_img_2.jpg'
+import profile_img_3 from './profile_img_3.jpg'
+
+export const assets = {
+    logo, 
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    project_img_5,
+    project_img_6,
+    left_arrow,
+    right_arrow,
+    profile_img_1,
+    profile_img_2,
+    profile_img_3
+}
+
+export const projectsData = [
+    {
+        title: "Portugal",
+        price: "$5000",
+        package: "5 days, 4 nights",
+        image: project_img_1
+    },
+    {
+        title: "England",
+        price: "$6000",
+        package: "4 days, 3 nights",
+        image: project_img_2
+    },
+    {
+        title: "Austria",
+        price: "$4000",
+        package: "6 days, 5 nights",
+        image: project_img_3
+    },
+    {
+        title: "Germany",
+        price: "$7000",
+        package: "4 days, 3 nights",
+        image: project_img_4
+    },
+    {
+        title: "Spain",
+        price: "$6500",
+        package: "3 days, 2 nights",
+        image: project_img_5
+    },
+    {
+        title: "France",
+        price: "$3000",
+        package: "3 days, 2 nights",
+        image: project_img_6
+    },
+];
+
+export const testimonialsData = [
+    {
+        name: "Reginald Watson",
+        title: "Manager",
+        image: profile_img_1,
+        alt: "Pic of Reginald Watson",
+        rating: 5,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius non dolor repellat ipsum minus assumenda blanditiis corporis tempore amet?",
+    },
+    {
+        name: "Reginald Watson",
+        title: "Manager",
+        image: profile_img_2,
+        alt: "Pic of Reginald Watson",
+        rating: 5,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius non dolor repellat ipsum minus assumenda blanditiis corporis tempore amet?",
+    },
+    {
+        name: "Reginald Watson",
+        title: "Manager",
+        image: profile_img_3,
+        alt: "Pic of Reginald Watson",
+        rating: 5,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius non dolor repellat ipsum minus assumenda blanditiis corporis tempore amet?",
+    },
+];
