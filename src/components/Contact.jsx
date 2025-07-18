ac94abddca3756eff44355d86a8d821047b01ddc
Archit Kayal <architkayal@gmail.com>
2025-07-18 22:39:32 +0530

    add animations using motion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { toast } from "react-toastify";
+import { motion } from "motion/react";
 
 const Contact = () => {
 	const [result, setResult] = React.useState("");
@@ -31,7 +32,8 @@ const Contact = () => {
 	};
 
 	return (
-		<div
+		<motion.div 
+			initial={{opacity:0, x:-200}} transition={{duration: 1}} whileInView={{opacity:1, x:0}} viewport={{once: true}}
 			className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
 			id="Contact"
 		>
@@ -81,7 +83,7 @@ const Contact = () => {
 					{result ? result : "Send Message"}
 				</button>
 			</form>
-		</div>
+		</motion.div>
 	);
 };
 
